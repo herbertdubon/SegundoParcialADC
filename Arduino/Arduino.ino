1b765b22c65f241a20a6fd3449129422b4e98f5e
herbertdubon <hdubon@tecnasa.com>
2018-04-15 13:36:58 -0600

    Modificando el codigo de node js.

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,74 +1,13 @@
 //Codigo Arduino
-<<<<<<< HEAD
-=======
-
->>>>>>> 28242e52a6485c02f45ef0256fd3887a57047b08
-int numReadings = 10;
-int readings[10];
-int index = 0;
-
-int sensorMax = 0;
-int sensorMin = 0;
 
 void setup()
 {
-  Serial.begin(9600);
-
-<<<<<<< HEAD
-  //pinMode(10, OUTPUT);
-  //pinMode(11, OUTPUT);
-=======
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
->>>>>>> 28242e52a6485c02f45ef0256fd3887a57047b08
-
-  sensorMax = analogRead(A0)/100;
-  sensorMin = sensorMax;
+  Serial.begin(9600);  
 }
 
 void loop()
 {
-  // MEASUREMENTS
-  // every iteration of loop makes one additional measurement,
-  // so the first 10 readings will display too low average
   int value = analogRead(A0)/100;
-<<<<<<< HEAD
-  //readings[index] = value;
-  //index++;
-  //if (index >= numReadings) index = 0; 
-=======
-  readings[index] = value;
-  index++;
-  if (index >= numReadings) index = 0; 
->>>>>>> 28242e52a6485c02f45ef0256fd3887a57047b08
-
-  // DO SOME MATH
-  if (value > sensorMax) sensorMax = value;
-  if (value < sensorMin) sensorMin = value;
-
-  //running average
-<<<<<<< HEAD
-  //float total = 0;
-  //for (int i = 0; i< numReadings; i++) total += readings[index];
-  //float average = total /numReadings;         
-=======
-  float total = 0;
-  for (int i = 0; i< numReadings; i++) total += readings[index];
-  float average = total /numReadings;         
->>>>>>> 28242e52a6485c02f45ef0256fd3887a57047b08
-
-  // OUTPUT TO SERIAL
-  Serial.print("\tMIN:\t");
-  Serial.print(sensorMin);
-  Serial.print("\tMAX:\t");
-  Serial.print(sensorMax);
-  Serial.print("\tACT:\t");
   Serial.println(value); 
-
-<<<<<<< HEAD
-  delay (500);
+  delay (1000);
 }
-=======
-  delay (100);
-}
->>>>>>> 28242e52a6485c02f45ef0256fd3887a57047b08
--- Telegram/index.js
+++ Telegram/index.js
@@ -1,1 +1,42 @@
 //Codigo de Telegram
+process.env["NTBA_FIX_319"] = 1;
+process.env["NTBA_FIX_319"] = 1;
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = '588640837:AAEvr4iQtJXWf1PC7KDflNibX8l9e0YcV90';
+
+var idChar = 211166639;
+var portName = process.argv[2];
+var SerialPort = require("serialport");
+var miSerial = new SerialPort('COM3', {
+   baudRate: 9600,
+   autoopen: true
+});
+
+const bot = new TelegramBot(token, {polling: true});
+
+
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Verificando Potenciómetro');
+  var respuesta = msg.text;
+  if (respuesta == 'encender')
+  {
+    miSerial.setEncoding('utf8');
+    miSerial.on('data', function(data)
+    {
+
+      //console.log('Data:', data);
+      var datanew = 10;
+      while (data < datanew) {
+        sensormin = data;
+        console.log('Data:', sensormin);
+        data = datanew;
+      }
+
+      //bot.sendMessage(idChar, "Sensor Max: " +sensormax);
+      //bot.sendMessage(idChar, "Sensor Min: " +sensormin);
+    });
+  }
+});
