77a5839171783ac7c5a4107b8a2c77ca0f6d1ecd
herbertdubon <hdubon@tecnasa.com>
2018-04-15 20:54:10 -0600

    Modificando codigo de node js.

--- Telegram/index.js
+++ Telegram/index.js
@@ -6,37 +6,35 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '588640837:AAEvr4iQtJXWf1PC7KDflNibX8l9e0YcV90';
 
 var idChar = 211166639;
+
 var portName = process.argv[2];
 var SerialPort = require("serialport");
 var miSerial = new SerialPort('COM3', {
-   baudRate: 9600,
-   autoopen: true
+  baudRate: 9600,
+  autoopen: true
 });
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, {
+  polling: true
+});
 
+miSerial.setEncoding('utf8');
 
+miSerial.on('data', function(data){
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Verificando Potenciómetro');
-  var respuesta = msg.text;
-  if (respuesta == 'encender')
-  {
-    miSerial.setEncoding('utf8');
-    miSerial.on('data', function(data)
-    {
+    var sensormax = 0;
+    var sensormin = 0;
 
-      //console.log('Data:', data);
-      var datanew = 10;
-      while (data < datanew) {
-        sensormin = data;
-        console.log('Data:', sensormin);
-        data = datanew;
-      }
+    Vactual = data.toString('utf8');
 
-      //bot.sendMessage(idChar, "Sensor Max: " +sensormax);
-      //bot.sendMessage(idChar, "Sensor Min: " +sensormin);
-    });
-  }
+    if (Vactual > sensormax)
+      sensormax = Vactual;
+
+    if (Vactual < sensormin)
+      sensormin = Vactual;
+    console.log("Sensormax: ", sensormax);
+    console.log("Sensormin: ", sensormin);
+
+    //bot.sendMessage(chatId, 'Verificando Potenciómetro');
+    //bot.sendMessage(chatId, data);
 });
