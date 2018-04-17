d2a54bfce5beca86c05e6bfcd0a877c12280d76b
herbertdubon <hdubon@tecnasa.com>
2018-04-16 23:34:15 -0600

    Se dejo el sensor tirando data entre 0 y 10

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,13 +1,12 @@
-//Codigo Arduino
 
-void setup()
-{
-  Serial.begin(9600);  
+
+void setup() {
+ Serial.begin(9600);
 }
 
-void loop()
-{
-  int value = analogRead(A0)/100;
-  Serial.println(value); 
-  delay (1000);
+void loop() {
+  
+int value =analogRead(A0)/100;
+Serial.println(value);
+delay(4000);
 }
